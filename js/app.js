cb3903cba98554e636ddaafa85be0bb7f4e416de
Noah Rhodes <noahmrhodes@gmail.com>
2019-08-27 21:24:32 -0700

    added index.html and a fishy picture

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var cookiesByStoreByHour = [];
 var totalCookiesByHour = [];
 
 var hoursOfOperation = ['6am','7am','8am','9am','10am','11am','12pm','1pm','2pm','3pm','4pm','5pm','6pm','7pm','8pm'];
-var controlCurve = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6]
+var controlCurve = [0.5, 0.75, 1.0, 0.6, 0.8, 1.0, 0.7, 0.4, 0.6, 0.9, 0.7, 0.5, 0.3, 0.4, 0.6];
 
 function getRandomIntInclusive(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min; 
